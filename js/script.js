0abfbaefb1b8db43cd65fff00925b525d892a399
Matheus <matheusedudu03@gmail.com>
2026-03-30 10:49:57 -0300

    estrutura basica do site que será utilizado para carregar usuarios da API

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const ul = document.querySelector("ul");
+
+
+async function buscarDados() {
+    try {
+        const result = await fetch("https://jsonplaceholder.typicode.com/users");
+        const dados = await result.json()
+
+        return dados;
+        
+    } catch (error) {
+        console.error("Erro", error);
+
+    }
+    
+}
+
+
+async function renderizar() {
+    const usuarios = await buscarDados();
+
+    try {
+        ul.innerHTML = "";
+        usuarios.forEach(element => {
+            const li = document.createElement("li");
+            li.innerHTML = element.name;
+
+            ul.appendChild(li)
+            
+        });
+    } catch (error) {
+        ul.innerHTML = `Erro ao renderizar ${error}`
+        
+    }
+}
+
+
+renderizar()
